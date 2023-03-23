b0a70d304af645a6dc76c95ac9743a5291a23f44
aboubakarsoulama <totypopito306@gmail.com>
2023-03-23 20:52:21 +0000

    2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,18 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  // name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
